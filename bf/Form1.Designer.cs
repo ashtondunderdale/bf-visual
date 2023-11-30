061f89cdcea945c19793ce7679bc036310fd08c2
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-30 16:12:20 +0000

    added pointer and data flow output

--- bf/Form1.Designer.cs
+++ bf/Form1.Designer.cs
@@ -16,13 +16,33 @@
         #region Windows Form Designer generated code
         private void InitializeComponent()
         {
+            inputBox = new RichTextBox();
+            outputBox = new RichTextBox();
             SuspendLayout();
             // 
-            // Form1
+            // inputBox
+            // 
+            inputBox.Location = new Point(12, 12);
+            inputBox.Name = "inputBox";
+            inputBox.Size = new Size(318, 110);
+            inputBox.TabIndex = 0;
+            inputBox.Text = "";
+            // 
+            // outputBox
+            // 
+            outputBox.Location = new Point(12, 128);
+            outputBox.Name = "outputBox";
+            outputBox.Size = new Size(776, 45);
+            outputBox.TabIndex = 1;
+            outputBox.Text = "";
+            // 
+            // Brainfuck
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(outputBox);
+            Controls.Add(inputBox);
             Name = "Brainfuck";
             Text = "Brainfuck";
             Load += Brainfuck_Load;
@@ -30,5 +50,8 @@
         }
 
         #endregion
+
+        private RichTextBox inputBox;
+        private RichTextBox outputBox;
     }
 }
--- bf/Form1.cs
+++ bf/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Printing;
+
 namespace bf
 {
     public partial class Brainfuck : Form
@@ -9,7 +11,44 @@ namespace bf
 
         private void Brainfuck_Load(object sender, EventArgs e)
         {
+            inputBox.TextChanged += InputBox_TextChanged;
+        }
 
+        private void InputBox_TextChanged(object sender, EventArgs e)
+        {
+            int dataPointer = 0;
+            byte[] tape = new byte[10]; //32768
+
+            outputBox.Text = "";
+
+            foreach (var command in inputBox.Text)
+            {
+                switch (command)
+                {
+                    case '>':
+                        dataPointer++;
+                        break;
+
+                    case '<':
+                        dataPointer--;
+                        break;
+
+                    case '+':
+                        if (dataPointer < tape.Length) tape[dataPointer]++;
+
+                        break;
+
+                    case '-':
+                        if (dataPointer < tape.Length) tape[dataPointer]--;
+                        break;
+                }
+            }
+
+            for (int i = 0; i < tape.Length; i++) 
+            {
+                outputBox.Text += tape[i];
+                outputBox.Text += " | ";
+            }
         }
     }
 }
